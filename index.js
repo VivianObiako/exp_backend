dcc96abc31c550ac34842c32735eb23efccc8dd6
Vivian Obiako <vivianobiako1@gmail.com>
2025-05-30 19:08:31 +0100

    update index.js

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ app.post('/send-email', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app; 
